07f331594974e21e23f9145fa018aab93157b5bf
Melissa Treviño <mely.trevic@gmail.com>
2018-04-19 18:33:56 -0500

    Header structure

--- src/header.php
+++ src/header.php
@@ -3,22 +3,35 @@
 <head>
 	<!-- Responsive meta tag -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta charset="UTF-8">
+	<meta charset="UTF-8">
 	
-    <title>CADHAC</title>
+	<title>CADHAC</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container">
-        <img src="img/cadhaclogo.svg" alt="CADHAC"/>
-        <p>English</p>
-        <button>Dona</button>
-        <ul class="nav">
-            <li>Sobre nosotros</li>
-            <li>Qué hacemos</li>
-            <li>Comunicados</li>
-            <li>Publicados</li>
-            <li>Desaparecidos</li>
-            <li>Contacto</li>
-        </ul>
-    </div>
+	<div class="container">
+		<header>
+			<div class="row">
+				<img src="img/cadhaclogo.svg" alt="CADHAC"/>
+				<p>English</p>
+				<button>Dona</button>
+			</div>
+			<div class="row">
+				<nav class="navbar navbar-expand-lg">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+						<div class="navbar-nav">
+							<a class="nav-item nav-link active" href="#">Sobre nosostros</a>
+							<a class="nav-item nav-link" href="#">Qu&eacute; hacemos</a>
+							<a class="nav-item nav-link" href="#">Comunicados</a>
+							<a class="nav-item nav-link" href="#">Publicados</a>
+							<a class="nav-item nav-link" href="#">Desaparecidos</a>
+							<a class="nav-item nav-link" href="#">Contacto</a>
+						</div>
+					</div>
+				</nav>
+			</div>	
+		</header>
+	</div>
