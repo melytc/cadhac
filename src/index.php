dcf78a26f19f556525464f197fe88522f6a83b9a
Melissa Treviño <mely.trevic@gmail.com>
2018-04-19 16:48:15 -0500

    Add dir listing

--- src/index.php
+++ src/index.php
@@ -1,6 +1,41 @@
-<?php
-# Header will be created in header.php
-require header.php;
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Project Site : CADHAC</title>
+	<link rel="stylesheet" type="text/css" href="css/basics.css" />
+</head>
+<body>
 
+	<header>
+		<article>
+			<a href="#" class="logo"><img src="/img/logo.svg"  alt="CADHAC"></a>
+		</article>
+	</header>
 
-?>
+	<article>
+		<table>
+			<thead><tr>
+				<th width="85%">Plantilla</th>
+				<th width="15%" align="center">HTML</th>
+			</tr></thead>
+			<tbody>
+				<?php 
+					$exclude = ['index.php', 'footer.php', 'header.php', 'sidebar.php', 'mensaje.php','includes.php',];
+					$files = scandir('./'); 
+					foreach ($files as $phpfile):
+						if((strpos($phpfile, '.html')|| strpos($phpfile, '.php')) && !in_array($phpfile, $exclude)):
+				?>
+					<tr>
+						<td><a href="<?= $phpfile ?>" target="_blank"><?= $phpfile ?></a></td>
+						<td><a href="<?= $phpfile ?>" target="_blank" class="glyphicon glyphicon-ok-circle"></a></td>
+					</tr>
+				<?php endif; endforeach; ?>
+			</tbody>
+		</table>
+	</article>
+</body>
+</html>
