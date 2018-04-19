e2e4f684371f92e9995b8f998ac6bd8a3fd6fa24
Melissa Treviño <mely.trevic@gmail.com>
2018-04-19 16:46:49 -0500

    Header progress

---
+++ src/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- Responsive meta tag -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+	
+    <title>CADHAC</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+</head>
+<body>
+    <ul class="nav">
+        <li>Sobre nosotros</li>
+        <li>Qué hacemos</li>
+        <li>Comunicados</li>
+        <li>Publicados</li>
+        <li>Desaparecidos</li>
+        <li>Contacto</li>
+    </ul>
+    <button>Dona</button>
+    <p>English</p>
+</body>
+</html>
--- src/index.php
+++ src/index.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>CADHAC</title>
-</head>
-<body>
-	<h1>Hello CADHAC</h1>
-</body>
-</html>
+<?php
+# Header will be created in header.php
+require header.php;
+
+
+?>
