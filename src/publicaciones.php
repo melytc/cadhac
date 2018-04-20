59c2d28d79b6e3770931e6dc277bdaa72c4b8e2c
Melissa Treviño <mely.trevic@gmail.com>
2018-04-20 02:04:16 -0500

    Clean code

--- src/publicaciones.php
+++ src/publicaciones.php
@@ -1,4 +1,5 @@
 <?php include 'header.php'; ?>
+
 <div class="publicaciones content">
 	<section class="banner img-bg bg-black-alpha">
 		<div class="container">
@@ -11,26 +12,25 @@
 			<h1>Publicaciones</h1>
 			<div class="pubslist">
 				<?php for($i = 0; $i < 4; $i++): ?>
-				<article class="publication">
-					<div class="row">
-						<div class="col-md-3">
-							<img src="img/temppub.jpg" alt="Titulo del documento">
+					<article class="publication">
+						<div class="row">
+							<div class="col-md-3">
+								<img src="img/temppub.jpg" alt="Titulo del documento">
+							</div>
+							<div class="col-md-9">
+								<p class="date ttu">Septiembre 24, 2017</p>
+								<h3 class="title">Desapariciones en Nuevo León desde la mirada de CADHAC: Informe 2009-2016</h3>
+								<p class="extract">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nostrum saepe, officia quam! Ea libero cum veniam, dignissimos quo, dolores sit laboriosam distinctio adipisci nobis omnis dolore sint similique ut perferendis asperiores illo optio eum earum amet sequi commodi maxime.</p>
+								<a href="#" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+							</div>
 						</div>
-						<div class="col-md-9">
-							<p class="date ttu">Septiembre 24, 2017</p>
-							<h3 class="title">Desapariciones en Nuevo León desde la mirada de CADHAC: Informe 2009-2016</h3>
-							<p class="extract">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nostrum saepe, officia quam! Ea libero cum veniam, dignissimos quo, dolores sit laboriosam distinctio adipisci nobis omnis dolore sint similique ut perferendis asperiores illo optio eum earum amet sequi commodi maxime.</p>
-							<a href="#" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
-						</div>
-					</div>
-					<hr>
-				</article>
+						<hr>
+					</article>
 				<?php endfor; ?>
 			</div>
 		</div>
 	</div>	
 </div>
-<!-- <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900" rel="stylesheet">  -->
 <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600,700" rel="stylesheet"> 
-<!-- <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,600,700" rel="stylesheet">  -->
+
 <?php include 'footer.php'; ?>
