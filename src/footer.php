e520ca4a00b6fd7f7d10e258d058ed9fe175e7df
Melissa Treviño <mely.trevic@gmail.com>
2018-04-20 15:51:34 -0500

    Add footer

--- src/footer.php
+++ src/footer.php
@@ -1,3 +1,18 @@
+	<section class="unete">
+		<div class="container-fluid nospace">
+			<div class="row">
+				<div class="col-md-5 img-bg" data-bg="img/pexels.jpg">
+				</div>
+				<div class="col-md-7 bg-primary">
+					<div class="padthis">
+						<h2 class="flight">&Uacute;nete</h2>
+						<p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint quos, accusantium quae quasi magnam! Magni, accusamus placeat vel, corporis esse corrupti. Aperiam dolorum sunt, enim. Assumenda reiciendis expedita dolorem explicabo illum fuga mollitia, in accusantium sapiente ex, sunt sit quae, quidem consequatur unde dolor id architecto amet velit iusto deleniti delectus! Odio quasi rem hic fugit, voluptatum facere minus impedit?</p>
+						<a href="#" class="btn bg-lightgreen btn-lg ttu donate-link"><strong>Dona</strong></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
---
+++ src/home.php
@@ -0,0 +1,7 @@
+<?php include 'header.php' ?>
+<div class="content home">
+	<div class="container">
+		
+	</div>	
+</div>
+<?php include 'footer.php' ?>
