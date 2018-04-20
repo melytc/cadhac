3258f6cb86367ccff2c17c456ff24b9fed988a24
Melissa Treviño <mely.trevic@gmail.com>
2018-04-20 15:34:54 -0500

    Finish main about us

--- src/aboutus.php
+++ src/aboutus.php
@@ -1,135 +1,104 @@
 <?php include "header.php" ?>
+<div class="aboutus content">
+	<section class="banner img-bg bg-black-alpha" data-bg="img/pexels.jpg">
+		<div class="container">
+			<h1 class="white">Sobre nosotros</h1>
+			<p class="subtitle white">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+		</div>
+	</section>
 
-<!-- <img src="http://lorempixel.com/1200/600/sports" /> -->
-<div class="container">
-	<div class="row">
-		<div class="desc col-6">
-			<h2>Qui&eacute;nes somos</h2>
-			<p><strong>Ciudadanos en Apoyo a los Derechos Humanos A.C.</strong> es una organizaci&oacute; civil y sin fines lucrativos que tiene como causa principal la defensa de los Derechos Humanos de las personas.</p>
+	<section class="one">
+		<div class="smaller-container">
+			<div class="row">
+				<div class="desc first col-6">
+					<h2>Qui&eacute;nes somos</h2>
+					<p><strong>Ciudadanos en Apoyo a los Derechos Humanos A.C.</strong> es una organizaci&oacute;n civil y sin fines lucrativos que tiene como causa principal la defensa de los Derechos Humanos de las personas.</p>
+				</div>
+			</div>
+			
+			<div class="row justify-content-end">
+				<div class="desc col-6 text-right">
+					<h2>Misi&oacute;n</h2>
+					<p>En CADHAC buscamos despertar la conciencia de la sociedad civil para que asuma como causa propia la defensa y promoci&oacute;n de los Derechos Humanos, contribuyendo as&iacute; a que la dignidad de la persona se respete en nuestra sociedad.</p>
+				</div>
+			</div>
 		</div>
-	</div>
-	
-	<div class="row justify-content-end">
-		<div class="desc col-6 text-right">
-			<h2>Misi&oacute;n</h2>
-			<p>En CADHAC buscamos despertar la conciencia de la sociedad civil para que asuma como causa propia la defensa y promoci&oacute;n de los Derechos Humanos, contribuyendo as&iacute; a que la dignidad de la persona se respete en nuestra sociedad.</p>
+	</section>
+
+	<section class="acknowledgements">
+		<div class="container-fluid">
+			<div class="row nog bg-darkblue">
+				<div class="col-5">
+					<img class="big-photo" src="img/hmnaConsuelo.jpg"></img>
+				</div>
+				<div class="col-7">
+					<div class="semblanza">
+						<h2>Hermana Consuelo Morales</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit veniam voluptate sit, molestias omnis expedita sed, eius animi hic eveniet minima fuga deserunt doloremque nesciunt! Nam temporibus voluptatem maxime quae ea hic, minus nulla? Tempora ut accusantium, molestias asperiores aliquam numquam odit error obcaecati nostrum voluptates ullam repellat alias quibusdam voluptatem, blanditiis nihil sed harum nulla temporibus laborum. Nobis dignissimos non beatae in corrupti. Ullam maxime eligendi accusantium illo reiciendis maiores dignissimos sit dolorem ipsa.</p>
+						
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt architecto officiis dolores dicta minima expedita necessitatibus fugit omnis voluptatibus sapiente, labore, officia deleniti deserunt sed quasi temporibus. Saepe, quos obcaecati.</p>
+						
+						<p>Escucha su TED talk <a href="https://www.youtube.com/watch?v=rLRH5EVGBVg">aqu&iacute;</a>.</p>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
+	</section>
+
+	<section class="reconocimientos">
+		<div class="container-fluid">
+			<div class="row nog">
+				<div class="col-md-5 img-bg" data-bg="img/pexels.jpg">
+					<!-- <img class="big-photo" src="img/pexels.jpg" alt="Title"> -->
+				</div>
+				<div class="col-md-7">
+					<div class="recs">
+						<h2>Reconocimientos</h2>
+						<ul class="awards-list">
+							<li class="award active">
+								<div class="row">
+									<div class="col-md-1">
+										<a href="#"><span class="circle"></span></a>
+									</div>
+									<div class="col-md-11">
+										<a href="#" class="title">2015 - Premio Nacional de DDHH</a>
+										<p class="desc">La Hermana Consuelo Morales recibe el Premio Nacional de Derecho Humanos, que otorga la Comisi&oacute;n Nacional de Derechos Humanos en México.</p>
+									</div>
+								</div>
+							</li>
+							<li class="award">
+								<div class="row">
+									<div class="col-md-1">
+										<a href="#"><span class="circle"></span></a>
+										
+									</div>
+									<div class="col-md-11">
+										<a href="#" class="title">2015 - Premio Nacional de DDHH</a>
+										<p class="desc">La Hermana Consuelo Morales recibe el Premio Nacional de Derecho Humanos, que otorga la Comisi&oacute;n Nacional de Derechos Humanos en México.</p>
+									</div>
+								</div>
+							</li>
+							<li class="award">
+								<div class="row">
+									<div class="col-md-1">
+										<a href="#"><span class="circle"></span></a>
+										
+									</div>
+									<div class="col-md-11">
+										<a href="#" class="title">2015 - Premio Nacional de DDHH</a>
+										<p class="desc">La Hermana Consuelo Morales recibe el Premio Nacional de Derecho Humanos, que otorga la Comisi&oacute;n Nacional de Derechos Humanos en México.</p>
+									</div>
+								</div>
+							</li>
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="organizacion">
+				
+	</section>
 </div>
-
-<section class="acknowledgements">
-	<div class="container row bg-darkblue">
-		<img class="col-5 big-photo" src="img/hmnaConsuelo.jpg"></img>
-		<div class="col-7">
-			<h2 class="text-white">La Hna. Consuelo Morales</h2>
-			<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit veniam voluptate sit, molestias omnis expedita sed, eius animi hic eveniet minima fuga deserunt doloremque nesciunt! Nam temporibus voluptatem maxime quae ea hic, minus nulla? Tempora ut accusantium, molestias asperiores aliquam numquam odit error obcaecati nostrum voluptates ullam repellat alias quibusdam voluptatem, blanditiis nihil sed harum nulla temporibus laborum. Nobis dignissimos non beatae in corrupti. Ullam maxime eligendi accusantium illo reiciendis maiores dignissimos sit dolorem ipsa.</p>
-
-			<p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt architecto officiis dolores dicta minima expedita necessitatibus fugit omnis voluptatibus sapiente, labore, officia deleniti deserunt sed quasi temporibus. Saepe, quos obcaecati.</p>
-
-			<p class="text-white">Escucha su TED talk <a href="https://www.youtube.com/watch?v=rLRH5EVGBVg">aquí</a>.
-			</p>
-		</div>
-	</div>
-	<div class="container row">
-		<!-- class="row" imagen aquí de los reconocimientos (dinámico) -->
-		<h2>Reconocimientos</h2>
-		<ul>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2015 - Premio Nacional de DDHH</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">La Hna. Consuelo Morales recibe el Premio Nacional de Derecho Humanos, que otorga la Comisi&oacute;n Nacional de Derechos Humanos en México.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2015 - Premio "Gilberto Bosques"</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">Premio Franco-Alemán de Derechos Humanos “Gilberto Bosques”, que las Embajadas de Alemania y Francia en M&eacute;xico otorgan desde el 2013.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2014 - Medalla al M&eacute;rito C&iacute;vico, "Presea Estado de Nuevo Le&oacute;"</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">La Hna. Consuelo Morales recibe la Medalla al M&eacute;rito C&iacute;vico “Presea Estado de Nuevo Le&oacute;n”, m&aacute;ximo reconocimiento que otorga Nuevo Le&oacute;n.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2014 - Reconocimiento por la Paz con Justicia y Dignidad</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">Reconocimiento a CADHAC del Movimiento por la Paz con Justicia y Dignidad (Chihuahua) por su trabajo en Nuevo Le&oacute;n.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2012 - Premio "Don Sergio M&eacute;ndez Arceo"</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">CADHAC recibe el premio “Don Sergio M&eacute;ndez Arceo”, el de mayor prestigio en M&eacute;xico para personas defensoras de derechos humanos y organizaciones de la sociedad civil.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2011 - Premio Alison Des Forges</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">Premio Alison Des Forges por Activismo Extraordinario, el más alto honor que otroga la organización internacional Human Rights Watch para la Hermana Consuelo Morales.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2010 - Premio Nacional por la Igualdad y la No Discriminaci&oacute;n</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">El Consejo Nacional para Prevenir la Discriminaci&oacute;n (CONAPRED) otorga el Premio Nacional por la Igualdad y la No Discriminaci&oacute;n a la Hermana Consuelo Morales.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2008 - Premio "Tata Vasco"</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">Premio “Tata Vasco” para CADHAC por su destacada labor en favor de los derechos humanos en Nuevo León.</p>
-					</div>
-				</div>
-			</li>
-			<li>
-				<div>
-					<div class="row">
-						<a href=""> <span></span> <p class="title">2008 - Reconocimiento de DDHH del Distrito Federal</p> </a>
-					</div>
-					<div class="row">
-						<p class="desc">CADHAC recibe el reconocimiento de la Comisión de Derechos Humanos del Distrito Federal.</p>
-					</div>
-				</div>
-			</li>
-		</ul>
-	</div>
-</section>
-
-<div class="container">
-	<!-- Conoce al equipo (áreas) -->
-</div>
-
 <?php include "footer.php" ?>
