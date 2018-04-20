ec6ea8bf75b86806cb24bd601f5a59f56f2a9303
Melissa Treviño <mely.trevic@gmail.com>
2018-04-20 00:18:31 -0500

    Aboutus main structure done

--- src/aboutus.php
+++ src/aboutus.php
@@ -1,2 +1,79 @@
 <?php include "header.php" ?>
+
+<!-- <img src="http://lorempixel.com/1200/600/sports" /> -->
+<div class="container">
+	<h2>Qui&eacute;nes somos</h2>
+	<p><strong>Ciudadanos en Apoyo a los Derechos Humanos A.C.</strong> es una organizaci&oacute; civil y sin fines lucrativos que tiene como causa principal la defensa de los Derechos Humanos de las personas.</p>
+	
+	<h2>Misi&oacute;n</h2>
+	<p>En CADHAC buscamos despertar la conciencia de la sociedad civil para que asuma como causa propia la defensa y promoci&oacute;n de los Derechos Humanos, contribuyendo as&iacute; a que la dignidad de la persona se respete en nuestra sociedad.</p>
+</div>
+
+<div class="container">
+	<!-- class="row" imagen aquí de la Hermana Consuelo -->
+	<h2 >La Hermana Consuelo</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+<div class="container">
+	<!-- class="row" imagen aquí de los reconocimientos (dinámico) -->
+	<h2>Reconocimientos</h2>
+	<ul>
+		<li>
+			<div>
+				<div class="row">
+					<a href=""> <span></span> <p class="title">2017 - Premio Nacional de DDHH</p> </a>
+				</div>
+				<div class="row">
+					<p class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quis, voluptatibus accusamus aliquam tempore ab temporibus veritatis inventore at iste!</p>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div>
+				<div class="row">
+					<a href=""> <span></span> <p class="title">2016 - Premio Nacional de DDHH</p> </a>
+				</div>
+				<div class="row">
+					<p class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quis, voluptatibus accusamus aliquam tempore ab temporibus veritatis inventore at iste!</p>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div>
+				<div class="row">
+					<a href=""> <span></span> <p class="title">2015 - Premio Nacional de DDHH</p> </a>
+				</div>
+				<div class="row">
+					<p class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quis, voluptatibus accusamus aliquam tempore ab temporibus veritatis inventore at iste!</p>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div>
+				<div class="row">
+					<a href=""> <span></span> <p class="title">2014 - Premio Nacional de DDHH</p> </a>
+				</div>
+				<div class="row">
+					<p class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quis, voluptatibus accusamus aliquam tempore ab temporibus veritatis inventore at iste!</p>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div>
+				<div class="row">
+					<a href=""> <span></span> <p class="title">2013 - Premio Nacional de DDHH</p> </a>
+				</div>
+				<div class="row">
+					<p class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quis, voluptatibus accusamus aliquam tempore ab temporibus veritatis inventore at iste!</p>
+				</div>
+			</div>
+		</li>
+	</ul>
+</div>
+
+<div class="container">
+	<!-- Conoce al equipo (áreas) -->
+</div>
+
 <?php include "footer.php" ?>
