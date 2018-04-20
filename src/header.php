42c6ab22a44d6df33eadbf40c77b511238872856
Melissa Treviño <mely.trevic@gmail.com>
2018-04-19 20:10:19 -0500

    Finish header styles

--- src/header.php
+++ src/header.php
@@ -1,37 +1,48 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+	<meta charset="UTF-8">
 	<!-- Responsive meta tag -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta charset="UTF-8">
 	
 	<title>CADHAC</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="container">
-		<header>
+	<header class="main">
+		<div class="container">
 			<div class="row">
-				<a href="#"><img src="img/cadhaclogo.svg" alt="CADHAC"/></a>
-				<a href="#">English</a>
-				<a href="#">Dona</a>
-			</div>
-			<div class="row">
-				<nav class="navbar navbar-expand-lg">
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-						<div class="navbar-nav">
-							<a class="nav-item nav-link active" href="#">Sobre nosostros</a>
-							<a class="nav-item nav-link" href="#">Qu&eacute; hacemos</a>
-							<a class="nav-item nav-link" href="#">Comunicados</a>
-							<a class="nav-item nav-link" href="#">Publicados</a>
-							<a class="nav-item nav-link" href="#">Desaparecidos</a>
-							<a class="nav-item nav-link" href="#">Contacto</a>
-						</div>
+				<div class="col">
+					<a class="logo" href="#"><img src="img/cadhaclogo.svg" alt="CADHAC"/></a>
+				</div>
+				<div class="col">
+					<div class="row justify-content-end align-items-center">
+						<a class="language ttu" href="#">English</a>
+						<a class="btn bg-accent ttu" href="#">Dona</a>
 					</div>
-				</nav>
-			</div>	
-		</header>
-	</div>
+				</div>
+			</div>
+		</div>
+		<section class="main-nav">
+			<div class="container">
+				<div class="row">
+					<nav class="navbar navbar-expand-lg">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+							<div class="navbar-nav">
+								<a class="nav-item nav-link ttu" href="#">Sobre nosostros</a>
+								<a class="nav-item nav-link ttu" href="#">Qu&eacute; hacemos</a>
+								<a class="nav-item nav-link ttu" href="#">Comunicados</a>
+								<a class="nav-item nav-link ttu" href="#">Publicados</a>
+								<a class="nav-item nav-link ttu" href="#">Desaparecidos</a>
+								<a class="nav-item nav-link ttu" href="#">Contacto</a>
+							</div>
+						</div>
+					</nav>
+				</div>	
+			</div>
+		</section>
+	</header>
