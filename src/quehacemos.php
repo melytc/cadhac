ebf1c4e11e624c3dddd2745b452785d0e54a80a5
Melissa Treviño <mely.trevic@gmail.com>
2018-04-22 23:48:17 -0500

    Finish Que hacemos HTML

---
+++ src/quehacemos.php
@@ -0,0 +1,116 @@
+<?php include 'header.php'; ?>
+<div class="content quehacemos">
+	<section class="banner img-bg bg-purple-alpha" data-bg="img/pexels.jpg">
+		<div class="container">
+			<h1>Qu&eacute; hacemos</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, mollitia.</p>
+		</div>
+	</section>
+	<section class="areanav">
+		<div class="container-fluid p-0">
+			<div class="row no-gutters">
+				<div class="col projnavitem tac">
+					Acompa&ntilde;amiento a familiares de personas desaparecidas
+				</div>
+				<div class="col projnavitem tac">
+					Seguridad
+				</div>
+				<div class="col projnavitem tac">
+					Asesor&iacute;a jur&iacute;dica
+				</div>
+				<div class="col projnavitem tac">
+					Penitenciario
+				</div>
+				<div class="col projnavitem tac">
+					Promoci&oacute;n y defensa de los Derechos Humanos
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="one">
+		<div class="container">
+			<div class="container-w90p">
+				<div class="row">
+					<div class="col tac">
+						<h2 class="secondary ttu"><strong>Qu&eacute; hacemos</strong></h2>
+					</div>
+					<div class="col">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quisquam cupiditate sint, obcaecati quasi eveniet dicta, qui natus, voluptatum voluptatem adipisci sunt maxime culpa eos est, vitae accusantium itaque cum nemo eaque tempora similique. Aliquid nihil iste laboriosam iure rem nisi repellendus enim, modi officia, veniam minus vel facilis quos. Ut commodi cum minus quod tempora id ipsa, illum iste maiores amet dolore dolorum unde! Cum ratione ea eos, soluta provident adipisci. Unde voluptatem quis commodi eveniet placeat, pariatur at!</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto iure possimus, quisquam perferendis atque animi? Quod excepturi quis temporibus dolorem maxime, ipsam non blanditiis quasi ab perspiciatis, suscipit ut fugit molestiae eaque sequi doloribus natus, aliquam voluptates nihil voluptatem delectus!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="project project-bg bg-lightgreen">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-7">
+					<article class="frame">
+						<h2 class="title">Project Name</h2>
+						<span class="topline white"></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi illum animi facilis, ducimus consequuntur ipsam placeat nemo non aperiam dolorum optio modi itaque earum eum voluptas sunt maxime cumque, ipsa amet quam vel in dolorem. Rerum earum, ex eius sed!</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit perspiciatis deleniti quos, ipsum adipisci quisquam soluta dolores quaerat laborum hic!</p>
+					</article>
+				</div>
+				<div class="col-md-5 photo-grid">
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="overlay-bg bg-lightgreen"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="project twocol">
+		<div class="container-w90p">
+			<h2 class="title mb-1">Seguridad</h2>
+			<div class="row">
+				<div class="col p-5">
+					<span class="topline purple"></span>
+					<h3 class="subtitle">Intervenci&oacute;n comunitaria</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda voluptas non vitae dolorum voluptate temporibus molestias esse repudiandae fuga debitis eos, distinctio maxime qui consequatur nobis omnis aspernatur sapiente.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae eum consequuntur eius rem et, expedita cumque quod placeat sequi sed.</p>
+				</div>
+				<div class="col p-5">
+					<span class="topline green"></span>
+					<h3 class="subtitle">Monitoreo Policial</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda voluptas non vitae dolorum voluptate temporibus molestias esse repudiandae fuga debitis eos, distinctio maxime qui consequatur nobis omnis aspernatur sapiente.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae eum consequuntur eius rem et, expedita cumque quod placeat sequi sed.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="project project-bg right bg-secondary">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-7 order-2">
+					<article class="frame">
+						<h2 class="title">Project Name</h2>
+						<p class="m-0"><span class="topline white"></span></p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi illum animi facilis, ducimus consequuntur ipsam placeat nemo non aperiam dolorum optio modi itaque earum eum voluptas sunt maxime cumque, ipsa amet quam vel in dolorem. Rerum earum, ex eius sed!</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit perspiciatis deleniti quos, ipsum adipisci quisquam soluta dolores quaerat laborum hic!</p>
+					</article>
+				</div>
+				<div class="col-md-5 order-1 photo-grid">
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="overlay-bg bg-secondary"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</div>
+<?php include 'footer.php'; ?>
