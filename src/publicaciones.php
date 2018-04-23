5d444d168fcdbd0e9e71be7b1b41b8edd3959c66
Melissa Treviño <mely.trevic@gmail.com>
2018-04-23 16:20:25 -0500

    Added missing download attribute to a tags.

--- src/publicaciones.php
+++ src/publicaciones.php
@@ -4,7 +4,7 @@
 	<section class="banner img-bg bg-black-alpha" data-bg="img/testbg.png">
 		<div class="container">
 			<h2>Informe de desaparici&oacute;n forzada en Nuevo Le&oacute;n de 2009 a 2016</h2>
-			<a href="publicaciones/desapariciones-nl-2009-a-2016.pdf" class="btn bg-purple2">Descarga el reporte</a>
+			<a href="publicaciones/desapariciones-nl-2009-a-2016.pdf" download class="btn bg-purple2">Descarga el reporte</a>
 		</div>
 	</section>
 	<div class="container">
@@ -20,7 +20,7 @@
 							<p class="date ttu">2016</p>
 							<h3 class="title">Desapariciones en Nuevo Le&oacute;n desde la mirada de CADHAC: Informe 2009-2016</h3>
 							<p class="extract">Desapariciones en Nuevo Le&oacute;n, desde la mirada de CADHAC: Informe 2009-2016 es un aporte fundamental, un referente y, por tanto, una lectura obligada de consulta. Por una parte, nos permite entender el contexto y desarrollo del fen&oacute;meno en dicha entidad y, por otra, nos da cuenta de la experiencia y los aportes que desde Ciudadanos en Apoyo a los Derechos Humanos, A. C. se han venido desarrollando en los &uacute;ltimos a&ntilde;os, tanto para recibir y documentar casos de personas desaparecidas como para brindar acompa&ntilde;amiento legal y psicosocial a los familiares de v&iacute;ctimas.</p>
-							<a href="publicaciones/desapariciones-nl-2009-a-2016.pdf" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+							<a href="publicaciones/desapariciones-nl-2009-a-2016.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
 					<hr>
@@ -35,7 +35,7 @@
 							<p class="date ttu">Marzo 28, 2017</p>
 							<h3 class="title">Protocolo del uso de la fuerza para polic&iacute;as municipales</h3>
 							<p class="extract">El presente protocolo tiene como objetivo regular el uso de la fuerza de las y los agentes policiales municipales, en cumplimiento de sus funciones constitucionales y legales, para que sea utilizada con estricto apego al marco normativo y para cumplir con las obligaciones constitucionales en materia de derechos humanos.</p>
-							<a href="publicaciones/protocolo-uso-fuerza.pdf" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+							<a href="publicaciones/protocolo-uso-fuerza.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
 					<hr>
@@ -50,7 +50,7 @@
 							<p class="date ttu">Noviembre, 2014</p>
 							<h3 class="title">Desaparici&oacute;n de personas en NL, sistematizaci&oacute;n de experiencias de b&uacute;squeda de la justicia y la verdad</h3>
 							<p class="extract">El objetivo de este documento es sistematizar la experiencia de estas pr&aacute;cticas, a trav&eacute;s de la explicaci&oacute;n detallada de los procesos que han permitido que las reuniones llevadas a cabo entre organizaciones civiles y la autoridad sean existosas y puedan servir de ejemplo a otros estados o pa&iacute;ses.</p>
-							<a href="publicaciones/desaparicion-personas-nl.pdf" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+							<a href="publicaciones/desaparicion-personas-nl.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
 					<hr>
