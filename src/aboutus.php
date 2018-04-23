c7bf2e9942cf0d8633ea2ff07b6c6f1faa044f71
Melissa Treviño <mely.trevic@gmail.com>
2018-04-23 15:15:58 -0500

    Changed banner and photos, added scrollbar to awards section.d

--- src/aboutus.php
+++ src/aboutus.php
@@ -1,6 +1,6 @@
 <?php include "header.php" ?>
 <div class="aboutus content">
-	<section class="banner img-bg bg-black-alpha" data-bg="img/pexels.jpg">
+	<section class="banner img-bg bg-black-alph" data-bg="img/sobrenosotros.jpg">
 		<div class="container">
 			<h1 class="white">Sobre nosotros</h1>
 			<p class="subtitle white">Luchamos d&iacute;a a d&iacute;a por los derechos humanos</p>
@@ -34,7 +34,7 @@
 				<div class="col-7">
 					<div class="semblanza">
 						<h2>Hermana Consuelo Morales</h2>
-						<p>Consuelo Gloria Morales Elizondo (Monterrey, Nuevo L&eacute;on; marzo de 1949)1 es una activista mexicana, fundadora y directora de la organizaci&oacute;n Ciudadanos en Apoyo a los Derechos Humanos, A.C. Su labor como defensora de las v&iactue;ctimas de los Derechos Humanos ha sido reconocida por diversas instituciones nacionales e internacionales como el Premio Nacional de Derechos Humanos de la Comisi&oacute;n Nacional de Derechos Humanos y el premio Alison Des Forges por Activismo Extraordinario de Human Rights Watch.</p>
+						<p>Consuelo Gloria Morales Elizondo (Monterrey, Nuevo L&eacute;on; marzo de 1949) es una activista mexicana, fundadora y directora de la organizaci&oacute;n Ciudadanos en Apoyo a los Derechos Humanos, A.C. Su labor como defensora de las v&iactue;ctimas de los Derechos Humanos ha sido reconocida por diversas instituciones nacionales e internacionales como el Premio Nacional de Derechos Humanos de la Comisi&oacute;n Nacional de Derechos Humanos y el premio Alison Des Forges por Activismo Extraordinario de Human Rights Watch.</p>
 						
 						<p>Tras a&ntilde;os de trabajo con comunidades ind&iacute;genas en Veracruz y ni&ntilde;os de la calle en la Ciudad de M&eacute;xico, la hermana Consuelo regresa a su ciudad natal y encuentra una comunidad plagada de abusos pero sin la protecci&oacute;n de alguna organizaci&oacute;n de Derechos Humanos.​ En 1993 forma parte del equipo fundador de la asociaci&oacute;n Ciudadanos en Apoyo de Derechos Humanos, para subsanar este vac&iacute;o y es su directora desde entonces.</p>
 						
@@ -48,11 +48,11 @@
 	<section class="reconocimientos">
 		<div class="container-fluid">
 			<div class="row nog">
-				<div class="col-md-5 img-bg" data-bg="img/pexels.jpg">
+				<div class="col-md-5 img-bg" data-bg="img/reconocimiento-1.jpg">
 					<!-- <img class="big-photo" src="img/pexels.jpg" alt="Title"> -->
 				</div>
 				<div class="col-md-7">
-					<div class="recs">
+					<div class="recs" data-spy="scroll">
 						<h2>Reconocimientos</h2>
 						<ul class="awards-list">
 							<li class="award active">
@@ -109,7 +109,7 @@
 										
 									</div>
 									<div class="col-md-11">
-										<a href="#" class="title">2012 - Premio "Don Sergio M&eacte;ndez Arceo</a>
+										<a href="#" class="title">2012 - Premio "Don Sergio M&eacute;ndez Arceo</a>
 										<p class="desc">CADHAC recibe el premio “Don Sergio M&eacute;ndez Arceo”, el de mayor prestigio en M&eacute;xico para personas defensoras de derechos humanos y organizaciones de la sociedad civil.</p>
 									</div>
 								</div>
