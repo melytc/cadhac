c2004ea05359c8aa232bb11327a379dc1b680e5e
Melissa Treviño <mely.trevic@gmail.com>
2018-04-23 12:50:59 -0500

    Changed error 404 image

--- src/error404.php
+++ src/error404.php
@@ -2,22 +2,22 @@
 
 <div class="error content">
 	<section class="banner img-bg bg-black-alpha" data-bg="img/error404.jpg">
-		<div class="container text-right">
-			<h1 class="white text-right">¡Nos encontraste!</h1>
-			<p class="white text-right">Desafortunadamente, tambi&eacute;n encontraste nuestra p&aacute;gina de error 404.</p>
-			<p class="white text-right">Lo que significa que la p&aacute;gina que est&aacute;s buscando no ha sido encontrada o ya no existe.</ph3>
+		<div class="container ">
+			<h1 class="white ">¡Nos encontraste!</h1>
+			<p class="white ">Desafortunadamente, tambi&eacute;n encontraste nuestra p&aacute;gina de error 404.</p>
+			<p class="white ">Lo que significa que la p&aacute;gina que est&aacute;s buscando no ha sido encontrada o ya no existe.</ph3>
 		</div>
 		<div class="container help">
-			<h2 class="white text-right">Tal vez quer&iacute;as:</h2>
+			<h2 class="white ">Tal vez quer&iacute;as:</h2>
 			<ul class="nav flex-column">
 				<li class="nav-item">
-					<a class="nav-link text-right" href="#">Hacer una donaci&oacute;n</a>
+					<a class="nav-link " href="#">Hacer una donaci&oacute;n</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link text-right" href="#">Ponerte en contacto con nosotros</a>
+					<a class="nav-link " href="#">Ponerte en contacto con nosotros</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link text-right" href="#">Buscar reportes de a&ntilde;os pasados</a>
+					<a class="nav-link " href="#">Buscar reportes de a&ntilde;os pasados</a>
 				</li>
 			</ul>
 		</div>
