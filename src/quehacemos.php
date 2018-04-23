48825e8eab82f9cfb9f4be9f974573b7d9b0b54d
Melissa Treviño <mely.trevic@gmail.com>
2018-04-23 14:03:22 -0500

    Content loaded. Missing photo fixes

--- src/quehacemos.php
+++ src/quehacemos.php
@@ -1,9 +1,9 @@
 <?php include 'header.php'; ?>
 <div class="content quehacemos">
-	<section class="banner img-bg bg-purple-alpha" data-bg="img/pexels.jpg">
+	<section class="banner img-bg bg-purple-alpha" data-bg="img/quehacemos/acompanamiento-2.jpg">
 		<div class="container">
 			<h1>Qu&eacute; hacemos</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, mollitia.</p>
+			<p>Conoce nuestros proyectos.</p>
 		</div>
 	</section>
 	<section class="areanav">
@@ -35,8 +35,11 @@
 						<h2 class="secondary ttu"><strong>Qu&eacute; hacemos</strong></h2>
 					</div>
 					<div class="col">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quisquam cupiditate sint, obcaecati quasi eveniet dicta, qui natus, voluptatum voluptatem adipisci sunt maxime culpa eos est, vitae accusantium itaque cum nemo eaque tempora similique. Aliquid nihil iste laboriosam iure rem nisi repellendus enim, modi officia, veniam minus vel facilis quos. Ut commodi cum minus quod tempora id ipsa, illum iste maiores amet dolore dolorum unde! Cum ratione ea eos, soluta provident adipisci. Unde voluptatem quis commodi eveniet placeat, pariatur at!</p>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto iure possimus, quisquam perferendis atque animi? Quod excepturi quis temporibus dolorem maxime, ipsam non blanditiis quasi ab perspiciatis, suscipit ut fugit molestiae eaque sequi doloribus natus, aliquam voluptates nihil voluptatem delectus!</p>
+						<p>CADHAC nace el 23 de abril de 1993 ante la urgencia de defender y promover los derechos fundamentales de hombres y mujeres desde una concepci&oacute;n integral. En un primer momento, CADHAC se gesta en el seno de comunidades eclesiales de base ubicadas Ciudad Guadalupe, Nuevo Le&oacute;n. Pronto, sin embargo, la magnitud del trabajo sum&oacute; a ciudadanos y ciudadanas que desde la laicidad impulsaban el respeto a la dignidad de la persona.</p>
+							
+						<p>Poco a poco, nuestra organizaci&oacute;n fue definiendo su perfil: el de una organizaci&oacute;n civil, apartidista, respetuosa de la pluralidad de credos, ideolog&iacute;as y preferencias sexuales, dedicada a la defensa y promoci&oacute;n de los Derechos Humanos.</p>
+						
+						<p>De 1993 a la fecha, CADHAC ha ampliado su &aacute;mbito de trabajo: de la difusi&oacute;n de los temas relacionados con los Derechos Humanos, se ha pasado a su defensa activa ante instancias jurisdiccionales y gubernamentales. Actualmente, nuestra organizaci&oacute;n es una de las ONG's m&aacute;s longevas en el norte de la Rep&uacute;blica. En el contexto estatal, nacional e internacional se reconoce a CADHAC su compromiso con la causa de los Derechos Humanos.</p>
 					</div>
 				</div>
 			</div>
@@ -47,20 +50,20 @@
 			<div class="row">
 				<div class="col-md-7">
 					<article class="frame">
-						<h2 class="title">Project Name</h2>
+						<h2 class="title">Acompa&ntilde;amiento a familias con personas desaparecidas</h2>
 						<span class="topline white"></span>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi illum animi facilis, ducimus consequuntur ipsam placeat nemo non aperiam dolorum optio modi itaque earum eum voluptas sunt maxime cumque, ipsa amet quam vel in dolorem. Rerum earum, ex eius sed!</p>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit perspiciatis deleniti quos, ipsum adipisci quisquam soluta dolores quaerat laborum hic!</p>
+						<p>CADHAC da acompa&ntilde;amiento psicosocial (social, psicol&oacute;gico y jur&iacute;dico) de manera gratuita a familias con personas desaparecidas en el estado de Nuevo Le&oacute;n y Tamaulipas. El colectivo de Agrupaci&oacute;n de Mujeres Organizadas por los Ejecutados Secuestrados y Desaparecidos de Nuevo Le&oacute;n (AMORES) se re&uacute;ne cada mi&eacute;rcoles en CADHAC. </p>
+						<p>Las familias tienen revisi&oacute;n de sus expedientes con la Fiscal&iacute;a de Nuevo Le&oacute;n y Tamaulipas. A su vez, se da terapia de juego a  ni&ntilde;os y ni&ntilde;as que tienen un familiar desaparecido. </p>
 					</article>
 				</div>
 				<div class="col-md-5 photo-grid">
 					<div class="row" style="height: 50%;">
-						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
-						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/quehacemos/acompanamiento-1.jpg"></div>
+						<div class="col img-bg" data-bg="img/quehacemos/acompanamiento-2.jpg"></div>
 					</div>
 					<div class="row" style="height: 50%;">
-						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
-						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/quehacemos/acompanamiento-3.jpg"></div>
+						<div class="col img-bg" data-bg="img/quehacemos/acompanamiento-4.jpg"></div>
 					</div>
 					<div class="overlay-bg bg-lightgreen"></div>
 				</div>
@@ -74,32 +77,57 @@
 				<div class="col p-5">
 					<span class="topline purple"></span>
 					<h3 class="subtitle">Intervenci&oacute;n comunitaria</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda voluptas non vitae dolorum voluptate temporibus molestias esse repudiandae fuga debitis eos, distinctio maxime qui consequatur nobis omnis aspernatur sapiente.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae eum consequuntur eius rem et, expedita cumque quod placeat sequi sed.</p>
+					<p>Se trabaja en dos colonias del municipio de Monterrey, San &Aacute;ngel Sur y Ejidal Los Remates, a trav&eacute;s del grupo vecinal “San &Aacute;ngel cambia contigo” en donde en coordinaci&oacute;n con el municipio se realizan actividades de empoderamiento ciudadano, recuperaci&oacute;n de espacios p&uacute;blicos y actividades en pro de la defensa y promoci&oacute;n de los derechos humanos.</p>
+					<p>Este proyecto tiene como objetivo a largo plazo reducir los &iacute;ndices de violencia e inseguridad en la colonia.</p>
 				</div>
 				<div class="col p-5">
 					<span class="topline green"></span>
 					<h3 class="subtitle">Monitoreo Policial</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda voluptas non vitae dolorum voluptate temporibus molestias esse repudiandae fuga debitis eos, distinctio maxime qui consequatur nobis omnis aspernatur sapiente.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae eum consequuntur eius rem et, expedita cumque quod placeat sequi sed.</p>
+					<p>A trav&eacute;s de entrevistas a personas detenidas por faltas administrativas en el centro de detenci&oacute;n Alamey, el municipio de Monterrey, se eval&uacute;a y analiza los proceso de detenci&oacute;n y el uso de la fuerza por polic&iacute;as municipales en acorde a lo establecido en el Protocolo Sobre Uso de la Fuerza. A su vez se dan talleres a policías en materia de derechos humanos. </p>
 				</div>
 			</div>
 		</div>
 	</section>
+	
 	<section class="project project-bg right bg-secondary">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-7 order-2">
 					<article class="frame">
-						<h2 class="title">Project Name</h2>
+						<h2 class="title">Asesoría jur&iacute;dica</h2>
 						<p class="m-0"><span class="topline white"></span></p>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi illum animi facilis, ducimus consequuntur ipsam placeat nemo non aperiam dolorum optio modi itaque earum eum voluptas sunt maxime cumque, ipsa amet quam vel in dolorem. Rerum earum, ex eius sed!</p>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit perspiciatis deleniti quos, ipsum adipisci quisquam soluta dolores quaerat laborum hic!</p>
+						<p>Se brinda asesor&iacute;a jur&iacute;dica gratuita a personas v&iacute;ctimas de violaciones a derechos humanos.</p>
+						<p>Entre ellas destacan casos de tortura, ejecuci&oacute;n extrajudicial, desaparici&oacute;n forzada, abuso de autoridad, entre otras. </p>
 					</article>
 				</div>
 				<div class="col-md-5 order-1 photo-grid">
 					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/quehacemos/juridico.jpg"></div>
 						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="overlay-bg bg-secondary"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section class="project project-bg right bg-lightgreen">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-7 order-2">
+					<article class="frame">
+						<h2 class="title">Penitenciario</h2>
+						<p class="m-0"><span class="topline white"></span></p>
+						<p>Se ofrece asesor&iacute;a gratuita y gestiones para personas privadas de la libertad en centros penitenciarios de Nuevo Le&oacute;n. Monitoreo de condiciones b&aacute;sicas.</p>
+					</article>
+				</div>
+				<div class="col-md-5 order-1 photo-grid">
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/quehacemos/penitenciario.jpg"></div>
 						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
 					</div>
 					<div class="row" style="height: 50%;">
@@ -112,5 +140,29 @@
 		</div>
 	</section>
 
+	<section class="project project-bg right bg-secondary">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-7 order-2">
+					<article class="frame">
+						<h2 class="title">Promoci&oacute;n y defensa de los derechos humanos</h2>
+						<p class="m-0"><span class="topline white"></span></p>
+						<p>Incidencia p&uacute;blica en temas de derechos humanos a nivel estatal y nacional, impulso de legislaci&oacute;n en materia de derechos humanos, conferencias y publicaciones en temas de derechos humanos, denuncia p&uacute;blica ante graves violaciones, monitoreo de organismos p&uacute;blicos como la Fiscal&iacute;a, Comisi&oacute;n Estatal a Derechos Humanos, Comisi&oacute;n Ejecutiva Estatal a Derechos Humanos, etc.  </p>
+					</article>
+				</div>
+				<div class="col-md-5 order-1 photo-grid">
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/quehacemos/promocionydefensa.jpg"></div>
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="row" style="height: 50%;">
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+						<div class="col img-bg" data-bg="img/pexels.jpg"></div>
+					</div>
+					<div class="overlay-bg bg-secondary"></div>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
 <?php include 'footer.php'; ?>
