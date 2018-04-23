f81059d83d2d512c7309a6f1f5ef4d39b9788b52
Melissa Treviño <mely.trevic@gmail.com>
2018-04-23 11:41:03 -0500

    Finished styling error page.

---
+++ src/error404.php
@@ -0,0 +1,27 @@
+<?php include "header.php" ?>
+
+<div class="error content">
+	<section class="banner img-bg bg-black-alpha" data-bg="img/error404.jpg">
+		<div class="container text-right">
+			<h1 class="white text-right">¡Nos encontraste!</h1>
+			<p class="white text-right">Desafortunadamente, tambi&eacute;n encontraste nuestra p&aacute;gina de error 404.</p>
+			<p class="white text-right">Lo que significa que la p&aacute;gina que est&aacute;s buscando no ha sido encontrada o ya no existe.</ph3>
+		</div>
+		<div class="container help">
+			<h2 class="white text-right">Tal vez quer&iacute;as:</h2>
+			<ul class="nav flex-column">
+				<li class="nav-item">
+					<a class="nav-link text-right" href="#">Hacer una donaci&oacute;n</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-right" href="#">Ponerte en contacto con nosotros</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-right" href="#">Buscar reportes de a&ntilde;os pasados</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+</div>
+
+<?php include "footer.php" ?>
