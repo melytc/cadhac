9274c56dae71579acc221b26df8b200d8ea703f4
Melissa Treviño <mely.trevic@gmail.com>
2018-04-23 13:21:47 -0500

    Content loaded.

--- src/publicaciones.php
+++ src/publicaciones.php
@@ -11,22 +11,50 @@
 		<div class="smaller-container">
 			<h1>Publicaciones</h1>
 			<div class="pubslist">
-				<?php for($i = 0; $i < 4; $i++): ?>
-					<article class="publication">
-						<div class="row">
-							<div class="col-md-3">
-								<img src="img/temppub.jpg" alt="Titulo del documento">
-							</div>
-							<div class="col-md-9">
-								<p class="date ttu">Septiembre 24, 2017</p>
-								<h3 class="title">Desapariciones en Nuevo León desde la mirada de CADHAC: Informe 2009-2016</h3>
-								<p class="extract">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nostrum saepe, officia quam! Ea libero cum veniam, dignissimos quo, dolores sit laboriosam distinctio adipisci nobis omnis dolore sint similique ut perferendis asperiores illo optio eum earum amet sequi commodi maxime.</p>
-								<a href="#" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
-							</div>
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/p-desapariciones en NL.png" alt="Titulo del documento">
 						</div>
-						<hr>
-					</article>
-				<?php endfor; ?>
+						<div class="col-md-9">
+							<p class="date ttu">2016</p>
+							<h3 class="title">Desapariciones en Nuevo Le&oacute;n desde la mirada de CADHAC: Informe 2009-2016</h3>
+							<p class="extract">Desapariciones en Nuevo Le&oacute;n, desde la mirada de CADHAC: Informe 2009-2016 es un aporte fundamental, un referente y, por tanto, una lectura obligada de consulta. Por una parte, nos permite entender el contexto y desarrollo del fen&oacute;meno en dicha entidad y, por otra, nos da cuenta de la experiencia y los aportes que desde Ciudadanos en Apoyo a los Derechos Humanos, A. C. se han venido desarrollando en los &uacute;ltimos a&ntilde;os, tanto para recibir y documentar casos de personas desaparecidas como para brindar acompa&ntilde;amiento legal y psicosocial a los familiares de v&iacute;ctimas.</p>
+							<a href="#" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/p-protocolo.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">Marzo 28, 2017</p>
+							<h3 class="title">Protocolo del uso de la fuerza para polic&iacute;as municipales</h3>
+							<p class="extract">El presente protocolo tiene como objetivo regular el uso de la fuerza de las y los agentes policiales municipales, en cumplimiento de sus funciones constitucionales y legales, para que  sea utilizada con estricto apego al marco normativo y para cumplir con las obligaciones constitucionales en materia de derechos humanos.</p>
+							<a href="#" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/p-desaparicion.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">Noviembre, 2014</p>
+							<h3 class="title">Desaparici&oacute;n de personas en NL, sistematizaci&oacute;n de experiencias de b&uacute;squeda de la justicia y la verdad</h3>
+							<p class="extract">El objetivo de este documento es sistematizar la experiencia de estas pr&aacute;cticas, a trav&eacute;s de la explicaci&oacute;n detallada de los procesos que han permitido que las reuniones llevadas a cabo entre organizaciones civiles y la autoridad sean existosas y puedan servir de ejemplo a otros estados o pa&iacute;ses.</p>
+							<a href="#" class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
 			</div>
 		</div>
 	</div>	
