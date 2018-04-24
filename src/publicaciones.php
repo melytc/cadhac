f0d693d1451b9c7ae0d1724d73c485224664ea37
Melissa Treviño <mely.trevic@gmail.com>
2018-04-24 15:43:24 -0500

    Finished adding publications from 2009-2016

--- src/publicaciones.php
+++ src/publicaciones.php
@@ -11,6 +11,22 @@
 		<div class="smaller-container">
 			<h1>Publicaciones</h1>
 			<div class="pubslist">
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/p-protocolo.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">Marzo 28, 2017</p>
+							<h3 class="title">Protocolo del uso de la fuerza para polic&iacute;as municipales</h3>
+							<p class="extract">El presente protocolo tiene como objetivo regular el uso de la fuerza de las y los agentes policiales municipales, en cumplimiento de sus funciones constitucionales y legales, para que sea utilizada con estricto apego al marco normativo y para cumplir con las obligaciones constitucionales en materia de derechos humanos.</p>
+							<a href="publicaciones/protocolo-uso-fuerza.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
 				<article class="publication">
 					<div class="row">
 						<div class="col-md-3">
@@ -29,13 +45,28 @@
 				<article class="publication">
 					<div class="row">
 						<div class="col-md-3">
-							<img src="img/p-protocolo.png" alt="Titulo del documento">
+							<img src="img/publicaciones/informe-2016.png" alt="Titulo del documento">
 						</div>
 						<div class="col-md-9">
-							<p class="date ttu">Marzo 28, 2017</p>
-							<h3 class="title">Protocolo del uso de la fuerza para polic&iacute;as municipales</h3>
-							<p class="extract">El presente protocolo tiene como objetivo regular el uso de la fuerza de las y los agentes policiales municipales, en cumplimiento de sus funciones constitucionales y legales, para que sea utilizada con estricto apego al marco normativo y para cumplir con las obligaciones constitucionales en materia de derechos humanos.</p>
-							<a href="publicaciones/protocolo-uso-fuerza.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+							<p class="date ttu">2016</p>
+							<h3 class="title">Informe de Actividades 2016</h3>
+							<p class="extract">La situaci&oacute;n de los derechos humanos en el Estado de Nuevo Le&oacute;n en 2016 tiene como indicativo que CADHAC recibi&oacute; 370 casos, 96 m&aacute;s que el a&ntilde;o anterior. En 2016, los casos en materia penal representan 68% del total de casos recibidos y estuvieron relacionados con problemas en centros penitenciarios, desapariciones, detenciones ilegales, abusos de autoridad y procesos judiciales irregulares, temas centrales en el trabajo de la organizaci&oacute;n.</p>
+							<a href="publicaciones/informe-2016.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/publicaciones/informe-2015.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">2015</p>
+							<h3 class="title">Informe de Actividades 2015</h3>
+							<p class="extract">En 2015, CADHAC recibi&oacute; 274 casos, 63 m&aacute;s que el a&ntilde;o anterior. Los casos en materia penal, que representan 55% del total de casos recibidos, refieren, principalmente, a desapariciones, problemas en centros penitenciarios, detenciones ilegales, abuso de autoridad y procesos judiciales, temas centrales en el trabajo que la organizaci&oacute;n desarroll&oacute; este a&ntilde;o.</p>
+							<a href="publicaciones/informe-2015.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
 					<hr>
@@ -59,12 +90,102 @@
 				<article class="publication">
 					<div class="row">
 						<div class="col-md-3">
+							<img src="img/publicaciones/informe-2014.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">2014</p>
+							<h3 class="title">Informe de Actividades 2014</h3>
+							<p class="extract">Tres temas fueron centrales para el trabajo que la organizaci&oacute;n desarroll&oacute; durante 2014: el sistema penitenciario, la tortura y las desapariciones (entre las que figuran desapariciones forzadas, privaci&oacute;n ilegal de la libertad, secuestros y desaparici&oacute;n). Estos tres &aacute;mbitos se caracterizan por mostrarnos las peores pr&aacute;cticas de las autoridades y su desd&eacute;n por los derechos humanos. En estos casos CADHAC trabaja para que haya justicia, acceso a la verdad, reconstrucci&oacute;n del tejido social y garant&iacute;as de no repetici&oacute;n.</p>
+							<a href="publicaciones/informe-2014.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/publicaciones/informe-2013.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">2013</p>
+							<h3 class="title">Informe de Actividades 2013</h3>
+							<p class="extract">El objetivo de este documento es mostrar la labor de esta organizaci&oacute;n a trav&eacute;s de las actividades realizadas en el a&ntilde;o 2013, las cuales concretan y reafirman su compromiso con la causa de los derechos humanos en nuestra comunidad. El documento se desglosa en cuatro partes, las cuales corresponden a sus &aacute;mbitos de acci&oacute;n: jur&iacute;dico, psicosocial, institucional y comunicaci&oacute;n.</p>
+							<a href="publicaciones/informe-2013.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/publicaciones/informe-2012.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">2012</p>
+							<h3 class="title">Informe de Actividades 2012</h3>
+							<p class="extract">En este sentido, el objetivo de este documento es mostrar la labor de esta organizaci&oacute;n a trav&eacute;s de las actividades realizadas en el a&ntilde;o 2012, las cuales concretan y reafirman su compromiso con la causa de los derechos humanos en nuestra comunidad. El documento se desglosa en cuatro partes, las cuales corresponden a sus &aacute;mbitos de acci&oacute;n: jur&iacute;dico, psicosocial y comunicaci&oacute;n. Al final se incluyen documentos de inter&eacute;s en Anexos.</p>
+							<a href="publicaciones/informe-2012.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/publicaciones/informe-2011.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">2011</p>
+							<h3 class="title">Informe de Actividades 2011</h3>
+							<p class="extract">Este documento da cuenta de la labor que la organizaci&oacute;n realiza a trav&eacute;s de actividades que concretizan y reafirman su compromiso con la causa de los derechos humanos en nuestra comunidad. Se reportan las actividades en el &aacute;rea jur&iacute;dica, la atenci&oacute;n psicol&oacute;gica a las v&iacute;ctimas, los esfuerzos de incidencia en el &aacute;mbito legislativo, el acompa&ntilde;amiento grupal a familiares v&iacute;ctimas y la coordinaci&oacute;n con otros actores sociales.</p>
+							<a href="publicaciones/informe-2011.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/publicaciones/informe-2010.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">2010</p>
+							<h3 class="title">Informe de Actividades 2010</h3>
+							<p class="extract">Monterrey y su &aacute;rea metropolitana, que durante muchos a&ntilde;os goz&oacute; fama de ser una de las mejores ciudades de Latinoam&eacute;rica para vivir, ahora es reconocido internacionalmente como un lugar peligroso y no recomendable para viajar. As&iacute; lo han boletinado gobiernos como el de Estados Unidos o Espa&ntilde;a. Y es que la presencia de la delincuencia organizada no gener&oacute; el clima de indefensi&oacute;n que reina en la Ciudad, sino tambi&eacute;n la infiltraci&oacute;n de criminales a las filas del gobierno, as&iacute; como la ineptitud de muchos funcionarios p&uacute;blicos que rigen sus administraciones bajo la batuta de la ocurrencia.</p>
+							<a href="publicaciones/informe-2010.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="img/publicaciones/informe-2009.png" alt="Titulo del documento">
+						</div>
+						<div class="col-md-9">
+							<p class="date ttu">2009</p>
+							<h3 class="title">Informe de Actividades 2009</h3>
+							<p class="extract">Durante el a&ntilde;o 2009 la situaci&oacute;n de derechos humanos continu&oacute; agrav&aacute;ndose, el trabajo en la materia sigue enfrent&aacute;ndose a serios obst&aacute;culos gubernamentales, como en a&ntilde;os anteriores, destacamos que muchas amenazas a los derechos humanos, como la presencia militar en las calles, los operativos inconstitucionales y la legitimaci&oacute;n de pr&aacute;cticas violatorias a los derechos humanos, han continuado.</p>
+							<a href="publicaciones/informe-2009.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
+						</div>
+					</div>
+					<hr>
+				</article>
+
+				<article class="publication">
+					<div class="row">
+						<div class="col-md-3">
 							<img src="img/publicaciones/informe-2008.png" alt="Titulo del documento">
 						</div>
 						<div class="col-md-9">
 							<p class="date ttu">2008</p>
 							<h3 class="title">Informe de Actividades 2008</h3>
-							<p class="extract">El año 2008, evidenció que en la entidad se atraviesa un momento difícil para el trabajo a favor de los derechos humanos. La ciudadanía percibe un grave deterioro en los niveles de seguridad pública, además de que ha sido afectada directamente por las políticas públicas que han emprendido las autoridades de los tres niveles de gobierno con pretexto del combate al narcotráfico. Sin temor a equivocarnos podemos apuntar que a partir del 2007, y con mayor impacto en el periodo descrito, se registran claros retrocesos en materia de DH en la entidad.</p>
+							<p class="extract">El a&ntilde;o 2008, evidenci&oacute; que en la entidad se atraviesa un momento dif&iacute;cil para el trabajo a favor de los derechos humanos. La ciudadan&iacute;a percibe un grave deterioro en los niveles de seguridad p&uacte;blica, adem&aacute;s de que ha sido afectada directamente por las pol&iacute;ticas p&uacte;blicas que han emprendido las autoridades de los tres niveles de gobierno con pretexto del combate al narcotr&aacute;fico. Sin temor a equivocarnos podemos apuntar que a partir del 2007, y con mayor impacto en el periodo descrito, se registran claros retrocesos en materia de DH en la entidad.</p>
 							<a href="publicaciones/informe-2008.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
@@ -79,7 +200,7 @@
 						<div class="col-md-9">
 							<p class="date ttu">2007</p>
 							<h3 class="title">Informe de Actividades, Segundo Semestre de 2007</h3>
-							<p class="extract">Durante el segundo semestre de 2007, continuaron manifestándose los peligros a los derechos humanos que detectamos desde principios de año, si bien la violencia relacionada con el narcotráfico disminuyó en este semestre, otras amenazas a los derechos humanos, como la presencia militar en las calles, los operativos inconstitucionales y la legitimación de prácticas violatorias a los derechos humanos, han continuado.</p>
+							<p class="extract">Durante el segundo semestre de 2007, continuaron manifest&aacute;ndose los peligros a los derechos humanos que detectamos desde principios de a&ntilde;o, si bien la violencia relacionada con el narcotr&aacute;fico disminuy&oacute; en este semestre, otras amenazas a los derechos humanos, como la presencia militar en las calles, los operativos inconstitucionales y la legitimaci&oacute;n de pr&aacute;cticas violatorias a los derechos humanos, han continuado.</p>
 							<a href="publicaciones/informe-2007.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
@@ -94,7 +215,7 @@
 						<div class="col-md-9">
 							<p class="date ttu">Enero, 2007</p>
 							<h3 class="title">Informe Anual 2006</h3>
-							<p class="extract">Durante el primer semestre de 2006 nos dimos cuenta de la necesidad de plantearnos nuestras formas de trabajar para lograr una cultura de respeto a los derechos humanos. Durante el segundo semestre de 2006, tomando en cuenta nuestras fuerzas y debilidades, consideremos que estamos en el momento oportuno para dar un paso cualitativo. Para ello hemos trabajado en nuestra planeación estratégica de donde concluimos la necesidad de focalizar nuestro esfuerzo.</p>
+							<p class="extract">Durante el primer semestre de 2006 nos dimos cuenta de la necesidad de plantearnos nuestras formas de trabajar para lograr una cultura de respeto a los derechos humanos. Durante el segundo semestre de 2006, tomando en cuenta nuestras fuerzas y debilidades, consideremos que estamos en el momento oportuno para dar un paso cualitativo. Para ello hemos trabajado en nuestra planeaci&oacute;n estrat&eacute;gica de donde concluimos la necesidad de focalizar nuestro esfuerzo.</p>
 							<a href="publicaciones/informe-2006.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
@@ -109,7 +230,7 @@
 						<div class="col-md-9">
 							<p class="date ttu">Enero, 2006</p>
 							<h3 class="title">Informe Anual 2005</h3>
-							<p class="extract">Como se desprende del presente informe, el 2005 ha sido un año rico en aprendizajes para Ciudadanos en Apoyo a los Derechos Humanos, A. C. El haber podido realizar eventos con presencia de actores nacionales en temas como niños, niñas y adolescentes, discriminación y pobreza, así como la elaboración del Diagnóstico de la Situación de los Derechos Humanos en el Sistema Penitenciario de Nuevo León, al mismo tiempo de fortalecer el quehacer cotidiano del respeto a los derechos humanos localmente y coordinar esfuerzos con otras organizaciones civiles tanto a nivel nacional como en la zona norte de México para enfrentar lo más inteligentemente posible los retos que con el incremento de violencia, presencia de narcotráfico, y el binomio corrupción impunidad, se nos presentan.</p>
+							<p class="extract">Como se desprende del presente informe, el 2005 ha sido un a&ntilde;o rico en aprendizajes para Ciudadanos en Apoyo a los Derechos Humanos, A. C. El haber podido realizar eventos con presencia de actores nacionales en temas como ni&ntilde;os, ni&ntilde;as y adolescentes, discriminaci&oacute;n y pobreza, as&iacute; como la elaboraci&oacute;n del Diagn&oacute;stico de la Situaci&oacute;n de los Derechos Humanos en el Sistema Penitenciario de Nuevo Le&oacute;n, al mismo tiempo de fortalecer el quehacer cotidiano del respeto a los derechos humanos localmente y coordinar esfuerzos con otras organizaciones civiles tanto a nivel nacional como en la zona norte de M&eacute;xico para enfrentar lo m&aacute;s inteligentemente posible los retos que con el incremento de violencia, presencia de narcotr&aacute;fico, y el binomio corrupci&oacute;n impunidad, se nos presentan.</p>
 							<a href="publicaciones/informe-2005.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
@@ -124,7 +245,7 @@
 						<div class="col-md-9">
 							<p class="date ttu">Enero, 2005</p>
 							<h3 class="title">Informe Anual 2004</h3>
-							<p class="extract">Como se desprende del presente informe, el 2004 ha sido un año rico en actividades y aprendizaje para Ciudadanos en Apoyo a los Derechos Humanos, A. C. El fortalecimiento de la organización, la profesionalización del equipo de trabajo y la permanencia de sus integrantes se han conjuntado con el prestigio moral acumulado tras años de labor para dar como resultado doce meses de trabajo intenso con algunos logros significativos. </p>
+							<p class="extract">Como se desprende del presente informe, el 2004 ha sido un a&ntilde;o rico en actividades y aprendizaje para Ciudadanos en Apoyo a los Derechos Humanos, A. C. El fortalecimiento de la organizaci&oacute;n, la profesionalizaci&oacute;n del equipo de trabajo y la permanencia de sus integrantes se han conjuntado con el prestigio moral acumulado tras a&ntilde;os de labor para dar como resultado doce meses de trabajo intenso con algunos logros significativos. </p>
 							<a href="publicaciones/informe-2004.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
@@ -139,7 +260,7 @@
 						<div class="col-md-9">
 							<p class="date ttu">Diciembre, 2003</p>
 							<h3 class="title">Informe Anual 2003</h3>
-							<p class="extract">El presente Informe Semestral de Actividades, Julio a Diciembre de 2003, describe las principales actividades, logros y desafíos en el trabajo de Ciudadanos en Apoyo a los Derechos Humanos, Asociación Civil.</p>
+							<p class="extract">El presente Informe Semestral de Actividades, Julio a Diciembre de 2003, describe las principales actividades, logros y desaf&iacute;os en el trabajo de Ciudadanos en Apoyo a los Derechos Humanos, Asociaci&oacute;n Civil.</p>
 							<a href="publicaciones/informe-2003.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
@@ -154,7 +275,7 @@
 						<div class="col-md-9">
 							<p class="date ttu">Diciembre, 2002</p>
 							<h3 class="title">Informe Anual 2002</h3>
-							<p class="extract">La crisis económica que cada día genera más pobreza, la injusticia estructural y sistemática, la violencia y la opresión, amenazan a la humanidad, sin embargo, día a día queremos fortalecer nuestra fé y esperanza de cambiar las cosas y contagiar cada vez a más personas con la convicción de que ejercer la dignidad propia y respetar a los otros, buscar la paz y la justicia, vale la pena, y que los signos que se nos presentan como las voces de hombres y mujeres de todo el mundo que se unen por la paz nos alumbra para seguir en este camino.</p>
+							<p class="extract">La crisis econ&oacute;mica que cada d&iacute;a genera m&aacute;s pobreza, la injusticia estructural y sistem&aacute;tica, la violencia y la opresi&oacute;n, amenazan a la humanidad, sin embargo, d&iacute;a a d&iacute;a queremos fortalecer nuestra fe y esperanza de cambiar las cosas y contagiar cada vez a m&aacute;s personas con la convicci&oacute;n de que ejercer la dignidad propia y respetar a los otros, buscar la paz y la justicia, vale la pena, y que los signos que se nos presentan como las voces de hombres y mujeres de todo el mundo que se unen por la paz nos alumbra para seguir en este camino.</p>
 							<a href="publicaciones/informe-2002.pdf" download class="btn download-report btn-primary bg-lightgreen">Descargar reporte</a>
 						</div>
 					</div>
